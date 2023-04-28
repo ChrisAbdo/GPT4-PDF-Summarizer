5ab7db8633b9dacf8f50ae1af3294352b6da56a5
ChrisAbdo <chris.j.abdo@gmail.com>
2023-04-28 01:59:51 -0400

    ADDED: Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,11 +4,14 @@ import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 import { Toaster } from "@/components/ui/toaster";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
       <Toaster />
+      <Analytics />
     </ThemeProvider>
   );
 }
