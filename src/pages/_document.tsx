2e10df10034659ff0a71091c91b498dae7320bf5
ChrisAbdo <chris.j.abdo@gmail.com>
2023-04-28 00:45:35 -0400

    ADDED: Updated SEO

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,37 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta name="description" content="Summarize any PDF in seconds." />
+          <meta
+            property="og:site_name"
+            content="https://pdfsummary.vercel.app/"
+          />
+          <meta
+            property="og:description"
+            content="Summarize any PDF in seconds."
+          />
+          <meta property="og:title" content="Summarize any PDF in seconds." />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content="Summarize any PDF in seconds." />
+          <meta
+            name="twitter:description"
+            content="Summarize any PDF in seconds."
+          />
+          <meta property="og:image" content="/image.jpeg" />
+          <meta name="twitter:image" content="/image.jpeg" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
